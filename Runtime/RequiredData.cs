f905ad7f31deab2dfc8c99726653b91ddd52f19f
Stiexeno <danka9536@gmail.com>
2022-11-19 19:17:30 +0200

    Added auto creating data

--- Runtime/RequiredData.cs
+++ Runtime/RequiredData.cs
@@ -2,20 +2,40 @@ using UnityEditor;
 using UnityEngine;
 using SF = UnityEngine.SerializeField;
 
-[CreateAssetMenu(menuName = "CustomEditor/RequiredData", fileName = "RequiredData")]
 public class RequiredData : ScriptableObject
 {
     private static RequiredData data;
 
-    public bool showIcon;
-    public bool showBorder;
+    public bool showIcon = true;
+    public bool showBorder = true;
+    
+    private const string LOCAL_PATH = "Assets/localdev";
     
     public static RequiredData Data
     {
         get
         {
             if (data == null)
-                data = (RequiredData)AssetDatabase.LoadAssetAtPath("Assets/RequiredAttribute/Data/RequiredData.asset", typeof(RequiredData));
+            {
+                if (data == null)
+                {
+                    if (AssetDatabase.IsValidFolder(LOCAL_PATH) == false)
+                    {
+                        AssetDatabase.CreateFolder("Assets", "localdev");
+                        
+                        RequiredData dataFile = CreateInstance<RequiredData>();
+                        string path = $"{LOCAL_PATH}/RequiredData.asset";
+                        AssetDatabase.CreateAsset(dataFile, path);
+                        AssetDatabase.SaveAssets();
+                        AssetDatabase.Refresh();
+
+                        data = dataFile;
+                        return data;
+                    }
+                    
+                    data = (RequiredData)AssetDatabase.LoadAssetAtPath($"{LOCAL_PATH}/RequiredData.asset", typeof(RequiredData));
+                }
+            }
 
             return data;
         }
